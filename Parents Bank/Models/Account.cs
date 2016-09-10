3671e87a56a350446fdfa16539c159b7fb7f1518
Abhay <badolaay@mail.uc.edu>
2016-09-09 23:14:38 -0400

    Added Account class

---
+++ Parents Bank/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parents_Bank.Models
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string Owner { get; set; }
+        public string Recipient { get; set; }
+        public string Name { get; set; }
+        public DateTime OpenDate { get; set; }
+        public double InterestRate { get; set; }
+
+    }
+}
